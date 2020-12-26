e9ea5060243e03a6108693bfc8199e7e6fd3c8da
hoomanhooman <hooman916@yahoo.com>
2020-12-27 00:17:34 +0330

    reset to empty project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
-import Vendor from './components/Vendor';
-import store from './store/store';
-import './styles/App.css';
+
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
         <div>
-          <Switch>
-            <Route path="/vendor-list" render={
-              (props)=>(
-                <Vendor {...props}/>
-              )
-            }/>
-            <Route path="/">
-              <Link className='vendor-link' to="/vendor-list">Vendor List</Link>
-            </Route>
-          </Switch> 
+           
         </div>
-      </Router>
-    </Provider>
   );
 }
 
--- src/sagaStore/saga/index.js
+++
@@ -1,9 +0,0 @@
-import {put, call, takeEvery} from 'redux-saga/effects';
-import axios from 'axios';
-
-import actions from '../action/vendorAction';
-
-function* getVendors(page,pageSize){
-    const res= yield call(axios.get,`https://snappfood.ir/mobile/v3/restaurant/vendors-list?page=${page}&page_size=${pageSize}`);
-    yield put(actions.getVendorSuccess(res.data.data.finalResult))
-}
